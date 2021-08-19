a39f02a2da818589d3c8f66ebf93ddb871f7b1fe
Syahrul Safarila <sysafarila.official@gmail.com>
2021-08-19 23:26:41 +0700

    update conditional image

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -13,11 +13,18 @@
                     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner">
                             @foreach ($product->images as $image)
-                            <div class="carousel-item @if($loop->first) active @endif">
-                                <img src="{{ asset('/storage/productImages/' . $image->image) }}" class="d-block w-100" alt="{{ asset('/storage/productImages/' . $image->image) }}" style="width: auto;
-                                height: 300px;
-                                object-fit: contain;">
-                            </div>
+                                @php
+                                    if (Storage::disk('local')->exists('public/productImages/' . $image->image)) {
+                                        $imagex = asset('/storage/productImages/' . $image->image);
+                                    } else {
+                                        $imagex = asset('/images/image-not-found.png');
+                                    }
+                                @endphp
+                                <div class="carousel-item @if ($loop->first) active @endif">
+                                    <img src="{{ $imagex }}" class="d-block w-100" alt="{{ $imagex }}" style="width: auto;
+                                        height: 300px;
+                                        object-fit: contain;">
+                                </div>
                             @endforeach
                         </div>
                         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -32,14 +39,16 @@
                     <div class="card-body">
                         <h5 class="card-title text-success font-weight-bold mb-1">{{ $product->name }}</h5>
                         <div class="d-flex justify-content-between mb-1">
-                            <h6 class="card-subtitle m-0 text-orange font-weight-bold">Rp {{ number_format($product->price,0 ,0, '.') }}</h6>
+                            <h6 class="card-subtitle m-0 text-orange font-weight-bold">Rp
+                                {{ number_format($product->price, 0, 0, '.') }}</h6>
                             {{-- <span class="badge badge-pill badge-success align-middle" style="white-space: pre;">Stock : {{ $product->stock }}</span> --}}
                         </div>
                         <p class="card-text">{{ $product->description }}</p>
                         @if (Auth::user()->role->name == 'buyer')
                             <form action="{{ route('carts.store') }}" method="post">
                                 @csrf
-                                <input type="number" name="quantity" id="quantity" class="form-control mb-2" value="1" min="1">
+                                <input type="number" name="quantity" id="quantity" class="form-control mb-2" value="1"
+                                    min="1">
                                 <input type="hidden" name="product_id" value="{{ $product->id }}">
                                 <button type="submit" class="btn btn-block btn-success">Add To Cart</button>
                             </form>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         @foreach ($products as $product)
+        @php
+            if (Storage::disk('local')->exists('public/productImages/' . $product->images[0]->image)) {
+                $image = asset('/storage/productImages/' . $product->images[0]->image);
+            } else {
+                $image = asset('/images/image-not-found.png');
+            }
+        @endphp
         <div class="col-6 px-1 col-md-3 col-lg-2 mb-2">
             <div class="card h-100 shadow-sm" id="">
-                <img src="{{ asset('/storage/productImages/' . $product->images[0]->image) }}" alt="" class="menus-image">
+                <img src="{{ $image }}" alt="" class="menus-image">
                 <div class="card-body p-2">
                     <h6 class="card-title menu-title-wrap"><a href="{{ route('products.show', $product->id) }}" class="stretched-link text-decoration-none text-success font-weight-bold">{{ $product->name }}</a></h6>
                     <h6 class="card-subtitle text-orange font-weight-bold">Rp {{ number_format($product->price,0 ,0, ',') }}</h6>
